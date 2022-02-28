4eb79dc92e70d97642bdcea10a1c4fd812e6bb37
JChlapowski <jchlapowski@uchicago.edu>
2022-02-28 00:06:40 -0600

    New Conv Structure

--- src/models/nhits/nhits.py
+++ src/models/nhits/nhits.py
@@ -145,7 +145,7 @@ class _NHITSBlock(nn.Module):
             self.pooling_layers = []
             i = 0
             self.divisor = 0
-            while 2**i <= self.n_pool_kernel_size:
+            while 2**i < self.n_pool_kernel_size:
                 self.divisor = 2**i
                 self.pooling_layers.append(nn.Conv1d(1, 1, kernel_size=self.n_pool_kernel_size, stride=math.ceil(self.n_pool_kernel_size/self.divisor)))
                 self.pooling_layers.append(activ)
