32ff7ee50651e9abc4de5af468ff6b88445d3388
JChlapowski <jchlapowski@uchicago.edu>
2022-03-04 20:18:36 -0600

    Adjusting dimensionsm

--- src/models/nhits/nhits.py
+++ src/models/nhits/nhits.py
@@ -215,9 +215,9 @@ class _NHITSBlock(nn.Module):
 
         theta = self.output_layer(hidden_theta.unsqueeze(1))
 
-        theta = theta.squeeze(1)
+        #theta = theta.squeeze(1)
 
-        print(theta)
+        print(theta.shape)
 
         backcast, forecast = self.basis(theta, insample_x_t, outsample_x_t)
 
